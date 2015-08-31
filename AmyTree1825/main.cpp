81c89b6bb35f1c7f7d4434defbf6424926b45f67
amytree1825 <nicmathias1825@iMac.local>
2015-08-31 10:34:04 -0400

    1088119948

--- AmyTree1825/main.cpp
+++ AmyTree1825/main.cpp
@@ -9,41 +9,19 @@ using namespace std;
 int main ()
 {
     time_t timer;
-    struct tm y2k;
+    struct tm epoch;
     double amytree1825;
     
-    //tm_sec
-    //Seconds after minute (0 – 59).
-    //tm_min
-    //Minutes after hour (0 – 59).
-    //tm_hour
-    //Hours after midnight (0 – 23).
-    //tm_mday
-    //Day of month (1 – 31).
-    //tm_mon
-    //Month (0 – 11; January = 0).
-    //tm_year
-    //Year (current year minus 1900).
-    //tm_wday
-    //Day of week (0 – 6; Sunday = 0).
-    //tm_yday
-    //Day of year (0 – 365; January 1 = 0).
-    //tm_isdst
-    //Positive value if daylight saving time is in effect; 0 if daylight saving time is not in effect; negative value if status of daylight saving time is unknown. If the TZ environment variable is set, the C run-time library assumes rules appropriate to the United States for implementing the calculation of daylight-saving time (DST).
+    epoch.tm_hour = 11; epoch.tm_min = 1; epoch.tm_sec = 0;
+    epoch.tm_year = 81; epoch.tm_mon = 2; epoch.tm_mday = 8;
+    epoch.tm_isdst = 1;
     
-    y2k.tm_hour = 11;   y2k.tm_min = 1; y2k.tm_sec = 0;
-    y2k.tm_year = 81; y2k.tm_mon = 2; y2k.tm_mday = 8;
-    //y2k.tm_wday = 2; y2k.tm_yday = 133;
-    y2k.tm_isdst = 1;
+    time(&timer);
+    amytree1825 = difftime(timer,mktime(&epoch));
     
-    //daylight savings time
-    time(&timer);  /* get current time; same as: timer = time(NULL)  */
-    
-    amytree1825 = difftime(timer,mktime(&y2k));
-    // got you
-    char wordsorword[3];
-    strncpy(wordsorword, "now", 3);
-    // the process to create a time has me anger, disgust, joy, fear, and sadness too. This was all based on the time from epock.
-    printf ("This epoch has a force greater than an altitude of %.f and has %s been accredited and has %s been certified for both space delta and ""space-x"" flight today by you.", amytree1825, wordsorword, wordsorword);
+    char word[3];
+    strncpy(word, "now", 3);
+    // PERSONA NON-GRATA
+    printf ("This version is %.f epoch and is %s certified and is %s accredited for ""space delta"" and ""space-x"" by CISSP.", amytree1825, word, word);
 }
 
